1393fe5b78952af76ed90b96c319a408532bcf28
ndemytruk <ndemytruk@gmail.com>
2020-02-06 12:32:20 +0100

    Chande: modifi data for testing numeric type and cultures

--- test-environments/react/my-app/src/App.js
+++ test-environments/react/my-app/src/App.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import Handsontable from 'handsontable';
-import { HotTable, HotColumn } from '@handsontable/react';
-import "handsontable/dist/handsontable.min.css";
+import { HotTable } from '@handsontable/react';
+import 'handsontable/dist/handsontable.min.css';
+import '../node_modules/numbro/languages/de-DE';
+import '../node_modules/numbro/languages/pl-PL';
+import 'handsontable/languages/all.js'
 
 
 class App extends React.Component {
@@ -9,47 +12,74 @@ class App extends React.Component {
     super(props);
 
     var dataObj = [
-      [1, -1, 'numbers', '=a1-b1', '12/1/2015', '=a1+b1', '=a1-b1', '=a1*b1'],
-      ['2', '20', 'strings converted', '=A2-B2', '5/12/2015', '=A2+B2', '=A2*B2', '=A2/B2'],
-      ['-1', '-10', 'string negatives', null, '10/23/2015', '=A3+B3', '=A3*B3', '=A3/B3'],
-      ['-1', '-10', 'string negatives', null, '10/23/2015', '=A4+B4', '=A4*B4', '=A4/B4'],
-      [0, 0, 'zero', 'zero', '12/1/2014', '=A5+B5', '=A5*B5', '=A5/B5'],
-      [-1, -10, 'negatives numbers', null, '12/1/2016', '=A5+B5', '=A2*B3', '=A2/B3'],
-      [-0, -0, 'negative zero', '', '12/1/2015', '=a6/a7', '=A6*B6', '=A6/B6'],
-      [0, -0, 'zero', null, '12/1/2014', '=A7+B7', '=A7*B7', '=A7/B7'],
-      [-1, -10, 'negatives numbers', null, '12/1/2016', '=A8^B8', '=A8*B8', '=A8/B8'],
-      [0.2, 0.1, 'numbers', null, '12/1/2015', '=a9/a9', '=A9*B9', '=A9/B9'],
-      [0.2, 0.1, 'strings_converted', null, '5/12/2015', '=A10+B10', '=A10*B10', '=A10/B10'],
-      [9999999999999999, 999999999999999, 'numbers, a1 should be 100..', null, '10/23/2015', '=A11+B11', '=A11*B11', '=A2/B3'],
-      ['500000.5', '500000,5', 'comma as separator', null, '12/1/2014', '=A4+B5', '=A12*B13', '=A12/B13'],
-      [50000000000000000.0, 0.000000001, 'big and small number', null, '12/1/2016', '=A13+B12', '=A13*B13', '=A13/B13']
+      [1, -1, 'numbers', '=a1-b1', '12/1/2015', '=a1+b1', '=a1-b1', '=a1*b1', '=a1*10'],
+      ['2', '20', 'strings converted', '=A2-B2', '5/12/2015', '=A2+B2', '=A2*B2', '=A2/B2', '=a2*10'],
+      ['-1', '-10', 'string negatives', null, '10/23/2015', '=A3+B3', '=A3*B3', '=A3/B3', '=a3*10'],
+      ['-1', '-10', 'string negatives', null, '10/23/2015', '=A4+B4', '=A4*B4', '=A4/B4', '=a4*10'],
+      [0, 0, 'zero', 'zero', '12/1/2014', '=A5+B5', '=A5*B5', '=A5/B5', '=a5*10'],
+      [-1, -10, 'negatives numbers', null, '12/1/2016', '=A6+B5', '=A6*B6', '=A6/B6', '=a6*10'],
+      [-0, -0, 'negative zero', '', '12/1/2015', '=a7/a7', '=A7*B7', '=A7/B7', '=a7*10'],
+      [0, -0, 'zero', null, '12/1/2014', '=A8+B8', '=A8*B8', '=A8/B8', '=a8*10'],
+      [-1, -10, 'negatives numbers', null, '12/1/2016', '=A9^B9', '=A9*B9', '=A9/B9'],
+      [0.2, 0.1, 'numbers', null, '12/1/2015', '=a10/a10', '=A10*B10', '=A10/B10'],
+      [0.2, 0.1, 'strings_converted', null, '5/12/2015', '=A11+B11', '=A11*B11', '=A11/B11'],
+      [9999999999999999, 999999999999999, 'numbers, a1 should be 100..', null, '10/23/2015', '=A12+B12', '=A12*B12', '=A12/B12'],
+      ['500000.5', '500000,5', 'comma as separator', null, '12/1/2014', '=A13+B13', '=A13*B13', '=A13/B13'],
+      [50000000000000000.0, 0.000000001, 'big and small number', null, '12/1/2016', '=A14+B14', '=A14*B14', '=A14/B14']
 
     ];
     this.state = {
       hotSettings: {
         data: dataObj,
-        colHeaders: true,
+        colHeaders: ['num1', 'num2', 'txt3', 'txt4', 'date5', 'num6', 'pric ()7', 'price-usd8', 'price-pl9'],
         rowHeaders: true,
         fixedRowsTop: 2,
         fixedRowsBottom: 2,
         fixedColumnsLeft: 2,
         contextMenu: true,
+        language: 'de-DE',
         licenseKey: 'non-commercial-and-evaluation',
         columns: [
-          { type: 'numeric' },
-          { type: 'numeric' },
-          { type: 'text' },
-          { type: 'text' },
-          { type: 'date', dateFormat: 'M/D/YYYY' },
-          { type: 'numeric' },
-          { type: 'numeric' },
-          { type: 'numeric' },
-          { type: 'numeric' },
+          { type: 'numeric' },    //1
+          { type: 'numeric' },    //2
+          { type: 'text' },     //3
+          { type: 'text' },     //4
+          { type: 'date', dateFormat: 'M/D/YYYY' },     //5
+          { type: 'numeric', 
+          numericFormat: {   //6
+            average: true,
+            totalLength: 5
+          } },      
+          { type: 'numeric',        //7
+          numericFormat: {
+            pattern: '0,0.00 $',
+            culture: 'de-DE'
+          } },
+          {
+            type: 'numeric',      //8
+            numericFormat: {
+              pattern: '$0,0.00',
+              culture: 'en-US'
+            }
+          },
+          {
+            type: 'numeric',      //9
+            numericFormat: {
+              pattern: '0,0.00 $',
+              culture: 'pl-PL'
+            }
+          }
         ],
         formulas: true,
+        manualRowResize: true,
+        manualColumnResize: true,
+        manualColumnMove: true,
+        manualRowMove: true,
+        allowEmpty: true
       }
     };
   }
+
 
   render() {
     return (
