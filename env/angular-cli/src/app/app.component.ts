e0596388aae4dd8b7e32f46644cf9109a55b6686
ndemytruk <ndemytruk@gmail.com>
2021-11-12 16:45:12 +0100

    Adds Angular-Cli environment

--- env/angular-cli/src/app/app.component.ts
+++ env/angular-cli/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import Handsontable from 'handsontable';
 
 @Component({
   selector: 'app-root',
@@ -6,18 +7,47 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
   template: `
   <div>
-    <hot-table
-      class="hot"
-      [data]="dataset"
-      [colHeaders]="true"
-      [rowHeaders]="true">
-        <hot-column data="id" [readOnly]="true" title="ID"></hot-column>
-        <hot-column data="name" title="Full name"></hot-column>
-        <hot-column data="address" title="Street name"></hot-column>
-    </hot-table>
+    <hot-table [settings]="hotSettings"></hot-table>
   </div>
   `,
 })
 export class AppComponent {
-  title = 'angular-cli';
+  title = 'hotv11';
+  hotSettings: Handsontable.GridSettings = {
+    data:
+      [
+        ['A1', 'https://handsontable.com/docs/10.0/img/examples/professional-javascript-developers-nicholas-zakas.jpg'],
+        ['A2', 'https://handsontable.com/docs/10.0/img/examples/javascript-the-good-parts.jpg']
+      ],
+    columns: [
+      {},
+      {
+        renderer(instance, td, row, col, prop, value, cellProperties) {
+          const escaped = Handsontable.helper.stringify(value);
+          let img = null;
+
+          if (escaped.indexOf('http') === 0) {
+            img = document.createElement('IMG');
+            img.src = value;
+
+            Handsontable.dom.addEvent(img, 'mousedown', event => {
+              event.preventDefault();
+            });
+
+            Handsontable.dom.empty(td);
+            td.appendChild(img);
+
+          } else {
+            Handsontable.renderers.TextRenderer.apply(this, arguments);
+          }
+
+          return td;
+        }
+      }
+    ],
+    colHeaders: true,
+    rowHeights: 55,
+    height: 'auto',
+    licenseKey: 'non-commercial-and-evaluation'
+  };
 }
--- env/angular-cli/src/app/app.module.ts
+++ env/angular-cli/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HotTableModule } from '@handsontable/angular';
 
+
 @NgModule({
   declarations: [
     AppComponent
